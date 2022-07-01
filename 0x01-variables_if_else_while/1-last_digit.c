bfc157c1a07528d65b40232105ed691c15d4af6f
OTochii <onyemstochi3@gmail.com>
2022-07-01 16:35:23 -0700

    commit for 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, digit;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+
+	printf("Last digit of %d is: %d\n", n, digit);
+	return (0);
+}
