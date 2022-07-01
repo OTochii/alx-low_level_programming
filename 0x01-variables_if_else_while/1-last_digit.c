528acb154802c3de8a419d686b46bddead80da61
OTochii <onyemstochi3@gmail.com>
2022-07-01 16:52:20 -0700

    commit for 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,6 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
 
-	printf("Last digit of %d is: %d\n", n, digit);
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	else if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	else if (n < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
