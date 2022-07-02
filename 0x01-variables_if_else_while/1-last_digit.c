b8722ac7956c185b9c8c1bcc3650e79cb19fc671
OTochii <onyemstochi3@gmail.com>
2022-07-01 17:51:10 -0700

    stress

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,17 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
 
-	if (n == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, digit);
-	}
-	else if (n != 0 && n > 5)
+	if (n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (n < 6)
+	else if (n < 6 && n != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
+	else 
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	return (0);
 }
