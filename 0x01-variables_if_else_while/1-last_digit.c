85e9b913a68ce82abd465063fecb418407f5bbeb
OTochii <onyemstochi3@gmail.com>
2022-07-01 17:42:38 -0700

    stress

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,16 @@ int main(void)
 	digit = n % 10;
 
 	if (n == 0)
+	{
 		printf("Last digit of %d is %d and is 0\n", n, digit);
+	}
 	else if (n != 0 && n > 5)
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	}
 	else if (n < 6)
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	}
 	return (0);
 }
