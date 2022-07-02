5cfae1c76b6323749564bc97e237dfb2697174e0
OTochii <onyemstochi3@gmail.com>
2022-07-01 17:34:09 -0700

    stress

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -12,15 +12,14 @@ int main(void)
 	int n, digit;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
 
 	if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, digit);
-	else if (n > 5 && n != 0)
+	else if (n != 0 && n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
-	else if (n < 6 && n != 0)
+	else if (n != 0 && n < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
