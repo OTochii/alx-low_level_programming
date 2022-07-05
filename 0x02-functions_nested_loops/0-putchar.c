af600e0d5467571f395a9661cce6e7f0a35275c2
OTochii <onyemstochi3@gmail.com>
2022-07-05 15:19:27 -0700

    commit for 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program to print _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
