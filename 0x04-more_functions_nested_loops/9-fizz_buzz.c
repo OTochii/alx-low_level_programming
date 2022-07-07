f6585126d1be6fd4a9bef3dc8f29ab97fcafc202
OTochii <onyemstochi3@gmail.com>
2022-07-07 16:01:05 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,9 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i == 100)
+			printf("Buzz");
+		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
