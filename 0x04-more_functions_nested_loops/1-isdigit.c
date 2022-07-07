93e85f5aabe8a82fd8874a469874c34a6f7d1a5a
OTochii <onyemstochi3@gmail.com>
2022-07-07 04:57:26 -0700

    commit for 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 
 /**
  * _isdigit - checks for a digit 0 through 9.
- * @c: charcter input
+ * @c: input
  *
  * Return: 1 if c is a digit, 0 if otherwise
  *
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
