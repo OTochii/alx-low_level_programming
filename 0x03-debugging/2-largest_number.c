81f654748fb5a1035b1e0f6fa0b1b3761db8dc51
OTochii <onyemstochi3@gmail.com>
2022-07-07 17:56:16 -0700

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b < c)
 	{
-		largest = b;
-	}
-	else if (b > a && a > c)
-	{
 		largest = a;
+	}
+	else if (b > a && a < c)
+	{
+		largest = b;
 	}
 	else
 	{
