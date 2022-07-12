28e89a2a2a50c599e5c9e31016fb359ab17504f5
OTochii <onyemstochi3@gmail.com>
2022-07-12 15:42:16 -0700

    commit for 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,29 +12,11 @@ void print_array(int *arr, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", arr[i]);
-		if (i == (n - 1))
-		{
-			printf("%d", arr[n - 1]);
-		}
+		printf("%d", arr[i]);
+		if (i < n - 1)
+			printf(", ");
 	}
-	printf("\n");
+	putchar('\n');
 }
-
-/**
- * print_array - using pointers to print items in an array
- * void print_array(int *arr, int n)
- * {
- * int i;
- *
- * for(i = 0; i < n; i++){
- * printf("%d", arr[i]);
- * if (i < n - 1){
- * printf(", ");
- * }
- * }
- * putchar('\n');
- * }
- */
