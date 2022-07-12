336844fa93a9222653e639e6640c6d22bad25655
OTochii <onyemstochi3@gmail.com>
2022-07-12 07:39:48 -0700

    commit for reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * reset_to_98: takes a pointer in memory to an int and updates/assigns it to the parameter 98, n then points 98.
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
