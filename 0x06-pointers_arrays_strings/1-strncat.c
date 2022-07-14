dcba7b2cfd28cf027ce699f52d35008e8ec88a9a
OTochii <onyemstochi3@gmail.com>
2022-07-14 22:01:11 +0000

    commit for task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings using at most n bytes from src
+ * @dest: string that accepts concatenated string from src
+ * @src: string to be concatenated
+ * @n: number of bytes used to concatenate src to dest
+ * Return: pointer to string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int srcLen, destLen = 0;
+
+	while (dest[destLen] != '\0')
+		destlen++;
+
+	for (srcLen = 0; src[srcLen] && srcLen < n; srcLen++)
+		dest[destLen++] = src[srcLen];
+
+	return (dest);
+}
