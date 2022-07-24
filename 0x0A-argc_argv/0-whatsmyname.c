7c18f801d6d186feee5f08808d4096e6326d99a0
OTochii <onyemstochi3@gmail.com>
2022-07-24 14:16:46 -0700

    update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,13 +7,8 @@
  *
  * Return: Always 0.
  */
-<<<<<<< HEAD
 
 int main(int __attribute((__unused__)) argc, char *argv[])
-
-=======
-int main(int __attribute((__unused__)) argc, char *argv[])
->>>>>>> 64892305f4d502901d44c18fc89f7a1c8682f469
 {
 	printf("%s\n", argv[0]);
 
