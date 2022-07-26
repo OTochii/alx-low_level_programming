cba7dbccd9f2cd1e56d706099e0b24fdaeaf481d
OTochii <onyemstochi3@gmail.com>
2022-07-26 13:32:55 -0700

    update for 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,17 @@
 char *_strdup(char *str)
 {
 	char *new_str;
-
-	new_str = str;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
+	for (index = 0; str[index] != '\0'; index++)
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+	if (duplicate == NULL)
+		return(NULL);
+	for (index = 0; str[index] != '\0'; index++)
+		duplicate[len] = '\0';
 
 	return (new_str);
 }
